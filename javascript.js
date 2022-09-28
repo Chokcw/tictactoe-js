cc425260a4e15691988624d25601fe636578f2a5
Chin Wee <chokcw@gmail.com>
2022-09-28 21:37:41 +0800

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,62 @@
+
+
+// game start - create grid with 9 boxes, array 0-8
+// board
+// array 0-8
+
+// check player turn
+
+
+// function GameBoard() {
+//     this.board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+// }
+
+
+
+// const gridFactory = () => {
+//     const grid = document.createElement("div");
+//     grid.classList.add("grid");
+
+//     // const markGrid = () => grid.textContent = "x";
+//     // const clearGrid = () => grid.textContent = "";
+//     return { sayHello };
+// };
+
+// function markGrid(grid) {
+
+// }
+
+
+
+const gameboard = (() => {
+    let _turn = 1;
+    let _boardArray = [["", "", ""], ["", "", ""], ["", "", ""]]
+
+    const displayBoard = () => {
+        const mainDiv = document.querySelector(".main");
+        const boardDiv = document.createElement("div");
+        boardDiv.classList.add("board");
+
+        let i = 0;
+        while (i < 9) {  
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            boardDiv.appendChild(grid);
+
+            i++;
+        };
+        mainDiv.appendChild(boardDiv);
+        // for x from 0 to 8, create a div and add to boardDiv
+        // add boardDiv to main
+    };
+
+    // check legal move
+    // check for winner
+    //reset
+    
+    return {
+        displayBoard,
+    };
+})();
+
+gameboard.displayBoard();
